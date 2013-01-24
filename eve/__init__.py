5df135abffda3599346b1ac51fb9bc0ef2415550
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-01-24 10:54:17 +0100

    Bumped version to 0.0.4

--- eve/__init__.py
+++ eve/__init__.py
@@ -10,7 +10,7 @@
     :license: BSD, see LICENSE for more details.
 """
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 #DEBUG = True
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ LONG_DESCRIPTION = open('README.rst').read()
 
 setup(
     name='Eve',
-    version='0.0.3',
+    version='0.0.4',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Nicola Iarocci',
