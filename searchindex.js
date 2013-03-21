1193efb1ef4d4ac956f1e1a3342d3e1611b2df15
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-03-21 11:55:33 +0100

    First commit of docs

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{origin:0,demo:0,readm:0,rest:0,instruct:0,api:0,check:0,out:0,welcom:0,progress:0,document:0,simpl:0,get:0,usag:0,capabl:0,"import":0,veri:0,along:0,hold:0,made:0,work:0,meanwhil:0},objtypes:{},titles:["Eve: RESTful APIs Made Simple"],objnames:{},filenames:["index"]})
