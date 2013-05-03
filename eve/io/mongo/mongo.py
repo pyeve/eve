082d5a339755dc93a326358b9e4757d4a713fcb3
Thomas Sileo <thomas.sileo@gmail.com>
2013-05-03 23:41:41 +0300

    Security issue with User-Restricted Resource Access
    I found a security issue, the query should be updated with the auth_username_field even when a GET with no parameters is performed, a user should only see the items he created.

--- eve/io/mongo/mongo.py
+++ eve/io/mongo/mongo.py
@@ -219,7 +219,7 @@ class Mongo(DataLayer):
         # if 'user-restricted resource access' is enabled and there's an Auth
         # request active, add the username field to the query
         username_field = config.DOMAIN[resource].get('auth_username_field')
-        if username_field and request.authorization and query:
+        if username_field and request.authorization:
             query.update({username_field: request.authorization.username})
 
         return datasource, query, fields
