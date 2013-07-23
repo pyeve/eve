d34193da73d0ca6411e300bf58de118ab05bd42a
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-07-23 11:37:34 +0200

    Py26 Fix: skip RateLimit tests if redis not available.
    Turns out 'unittest.skipIf' is not supported by Python 2.6. This
    implementation is also nicer as it does not need a global variable.

--- eve/tests/methods/ratelimit.py
+++ eve/tests/methods/ratelimit.py
@@ -1,46 +1,32 @@
 from eve.tests import TestBase
 import time
-import unittest
-
-# necessary evil
-my_redis = None
-try:
-    from redis import Redis, ConnectionError
-    my_redis = Redis()
-    try:
-        my_redis.flushdb()
-    except ConnectionError:
-        my_redis = None
-except ImportError:
-    pass
-
-print my_redis
 
 
 class TestRateLimit(TestBase):
     def setUp(self):
         super(TestRateLimit, self).setUp()
-        if my_redis:
-            self.app.redis = my_redis
+        try:
+            from redis import Redis, ConnectionError
+            self.app.redis = Redis()
+            try:
+                self.app.redis.flushdb()
+            except ConnectionError:
+                self.app.redis = None
+        except ImportError:
+            self.app.redis = None
+
+        if self.app.redis:
             self.app.config['RATE_LIMIT_GET'] = (1, 1)
 
-    @unittest.skipIf(my_redis is None, 'requires "pip install redis" and a '
-                     'running redis-server')
     def test_ratelimit_home(self):
-        self.get_ratelimit("/")
+            self.get_ratelimit("/")
 
-    @unittest.skipIf(my_redis is None, 'requires "pip install redis" and a '
-                     'running redis-server')
     def test_ratelimit_resource(self):
         self.get_ratelimit(self.known_resource_url)
 
-    @unittest.skipIf(my_redis is None, 'requires "pip install redis" and a '
-                     'running redis-server')
     def test_ratelimit_item(self):
         self.get_ratelimit(self.item_id_url)
 
-    @unittest.skipIf(my_redis is None, 'requires "pip install redis" and a '
-                     'running redis-server')
     def test_noratelimits(self):
         self.app.config['RATE_LIMIT_GET'] = None
         if self.app.redis:
@@ -52,13 +38,17 @@ class TestRateLimit(TestBase):
         self.assertTrue('X-RateLimit-Reset' not in r.headers)
 
     def get_ratelimit(self, url):
-        self.assertRateLimit(self.test_client.get(url))
-        r = self.test_client.get(url)
-        self.assertEqual(r.status_code, 429)
-        self.assertTrue('Rate limit exceeded' in r.data)
+        if self.app.redis:
+            self.assertRateLimit(self.test_client.get(url))
+            r = self.test_client.get(url)
+            self.assertEqual(r.status_code, 429)
+            self.assertTrue('Rate limit exceeded' in r.data)
 
-        time.sleep(1)
-        self.assertRateLimit(self.test_client.get(url))
+            time.sleep(1)
+            self.assertRateLimit(self.test_client.get(url))
+        else:
+            print ("Skipped. Needs a running redis-server and 'pip install "
+                   "redis'")
 
     def assertRateLimit(self, r):
         self.assertTrue('X-RateLimit-Remaining' in r.headers)
