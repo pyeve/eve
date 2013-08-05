19d0eae5145894ff57d5fddb16c4e7cc169f8334
rxl <me@ryanshea.org>
2013-08-04 23:37:52 -0400

    switch event hook lines

--- eve/methods/get.py
+++ eve/methods/get.py
@@ -161,9 +161,9 @@ def getitem(resource, **lookup):
         # state on the database).
         item_title = config.DOMAIN[resource]['item_title'].lower()
 
+        getattr(app, "on_getting_%s" % item_title)(document[config.ID_FIELD], document)
         getattr(app, "on_getting_item")(resource, document[config.ID_FIELD],
                                         document)
-        getattr(app, "on_getting_%s" % item_title)(document[config.ID_FIELD], document)
 
         response.update(document)
         return response, last_modified, document['etag'], 200
