7ab7b95e309919b73d4243edfa1006765421db66
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-09-03 16:58:01 +0200

    Tests for optional HATEOAS.

--- eve/tests/response.py
+++ eve/tests/response.py
@@ -2,6 +2,7 @@
 
 from ast import literal_eval
 from eve.tests import TestBase
+import simplejson as json
 
 
 class TestResponse(TestBase):
@@ -27,3 +28,30 @@ class TestResponse(TestBase):
         self.assertTrue(isinstance(resource, list))
         links = response.get('_links')
         self.assertTrue(isinstance(links, dict))
+
+
+class TestNoHateoas(TestBase):
+
+    def setUp(self):
+        super(TestNoHateoas, self).setUp()
+        self.app.config['HATEOAS'] = False
+        self.domain[self.known_resource]['hateoas'] = False
+
+    def test_no_hateoas_resource(self):
+        r = self.test_client.get(self.known_resource_url)
+        response = json.loads(r.get_data().decode())
+        self.assertTrue(isinstance(response, list))
+        self.assertEqual(len(response), 25)
+        item = response[0]
+        self.assertTrue(isinstance(item, dict))
+        self.assertTrue('_links' not in item)
+
+    def test_no_hateoas_item(self):
+        r = self.test_client.get(self.item_id_url)
+        response = json.loads(r.get_data().decode())
+        self.assertTrue(isinstance(response, dict))
+        self.assertTrue('_links' not in response)
+
+    def test_no_hateoas_homepage(self):
+        r = self.test_client.get('/')
+        self.assert404(r.status_code)
