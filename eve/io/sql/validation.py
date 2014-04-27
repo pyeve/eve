f75fe920e6e737659be7badee086dcf8c7c14a87
Jacopo <dedalusj@gmail.com>
2014-04-27 15:16:22 +1000

    Improved validation for post requests

--- eve/io/sql/validation.py
+++ eve/io/sql/validation.py
@@ -29,29 +29,25 @@ class ValidatorSQL(Validator):
     """
     def __init__(self, schema, resource=None):
         self.resource = resource
-        self.object_id = None
-        super(ValidatorSQL, self).__init__(schema, transparent_schema_rules=True)
+        self._id = None
+        super(ValidatorSQL, self).__init__(schema, transparent_schema_rules=True, allow_unknown=False)
 
-    def validate_update(self, document, object_id):
-        """ Validate method to be invoked when performing an update, not an
-        insert.
-
-        :param document: the document to be validated.
-        :param object_id: the unique id of the document.
-        """
-        self.object_id = object_id
-        return super(ValidatorSQL, self).validate_update(document)
-
-    def validate_replace(self, document, _id):
-        self._id = _id
-        if super(ValidatorSQL, self).validate(document) is False:
+    def validate(self, document, schema=None, update=False):
+        if super(ValidatorSQL, self).validate(document, schema, update) is False:
             return False
         # SQL is a fixed schema db so query for rows where fields haven't been set return None or null in json
-        # In order for the etag computation to work when replacing entries with incomplete documents we need
+        # In order for the etag computation to work for entries with incomplete fields, we need
         # to insert the missing fields as None
         document.update({k: None for k in self.schema.keys() if k not in document and k != '_id'})
         return True
 
+    def validate_update(self, document, _id):
+        self._id = _id
+        return super(ValidatorSQL, self).validate_update(document)
+
+    def validate_replace(self, document, _id):
+        self._id = _id
+        return self.validate(document)
 
     def _validate_unique(self, unique, field, value):
         """ Enables validation for `unique` schema attribute.
@@ -97,14 +93,3 @@ class ValidatorSQL(Validator):
             if not app.data.find_one(data_relation['resource'], None, **query):
                 self._error(field, "value '%s' must exist in resource '%s', field '%s'." %
                             (value, data_relation['resource'], data_relation['field']))
-
-    def _validate_type_objectid(self, field, value):
-        """ Enables validation for `objectid` schema attribute.
-
-        :param unique: Boolean, wether the field value should be
-                       unique or not.
-        :param field: field name.
-        :param value: field value.
-        """
-        # TODO ?
-        pass
