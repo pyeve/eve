bbc2e108fb3b01177ab62d7204b5063ceefc56b5
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-05-15 10:48:39 +0200

    Add Python 3.4 to trove qualifiers.
    And fix the classifiers list.

--- setup.py
+++ setup.py
@@ -38,11 +38,12 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2'
-        'Programming Language :: Python :: 2.6'
-        'Programming Language :: Python :: 2.7'
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
