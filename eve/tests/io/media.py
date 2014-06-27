pyeve/eve

commit ad4afe90924241e907e94824b13294bef2c0b217

Don't try to delete a field that doesn't exist.

eve/methods/common.py
@@ -366,7 +366,7 @@ def resolve_media_files(document, resource, original=None):
     # document update fails we should probably attempt a cleanup on the storage
     # sytem. Easier said than done though.
     for field in resource_media_fields(document, resource):
-        if original:
+        if original and hasattr(original, field):
             # since file replacement is not supported by the media storage
             # system, we first need to delete the file being replaced.
             app.media.delete(original[field])

commit 57d3834bb1f73c2076c09d83036336c7fd034a0d

Fix broken media tests for PUT and PATCH.
Also fixed wrong check introduced in ad4afe90924241e907e94824b13294bef2c0b217.

eve/methods/common.py
@@ -578,7 +578,7 @@ def store_media_files(document, resource, original=None):
     # document update fails we should probably attempt a cleanup on the storage
     # sytem. Easier said than done though.
     for field in resource_media_fields(document, resource):
-        if original and hasattr(original, field):
+        if original and field in original:
             # since file replacement is not supported by the media storage
             # system, we first need to delete the file being replaced.
             app.media.delete(original[field])

eve/tests/io/media.py
@@ -157,8 +157,11 @@ class TestGridFSMediaStorage(TestBase):
         _id = r[ID_FIELD]
         etag = r[ETAG]
 
-        # retrieve media_id and compare original and returned data
-        media_id = self.assertMediaField(_id, self.encoded, self.clean)
+        # compare original and returned data
+        self.assertMediaField(_id, self.encoded, self.clean)
+
+        # retrieve media_id
+        media_id = self.assertMediaStored(_id)
 
         # PUT replaces the file with new one
         clean = b'my new file contents'
@@ -189,8 +192,11 @@ class TestGridFSMediaStorage(TestBase):
         _id = r[ID_FIELD]
         etag = r[ETAG]
 
-        # retrieve media_id and compare original and returned data
-        media_id = self.assertMediaField(_id, self.encoded, self.clean)
+        # compare original and returned data
+        self.assertMediaField(_id, self.encoded, self.clean)
+
+        # retrieve media_id
+        media_id = self.assertMediaStored(_id)
 
         # PATCH replaces the file with new one
         clean = b'my new file contents'
@@ -219,7 +225,9 @@ class TestGridFSMediaStorage(TestBase):
         etag = r[ETAG]
 
         # retrieve media_id and compare original and returned data
-        media_id = self.assertMediaField(_id, self.encoded, self.clean)
+        self.assertMediaField(_id, self.encoded, self.clean)
+
+        media_id = self.assertMediaStored(_id)
 
         # DELETE deletes both the document and the media file
         headers = [('If-Match', etag)]