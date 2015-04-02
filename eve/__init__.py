694325e29307571d20e1a2c006f22ddfad69fca2
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-04-02 10:46:54 +0200

    Bump version to 0.6-dev0

--- eve/__init__.py
+++ eve/__init__.py
@@ -38,7 +38,7 @@
 
 """
 
-__version__ = '0.5.4-dev'
+__version__ = '0.6-dev0'
 
 # RFC 1123 (ex RFC 822)
 DATE_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except ImportError:
 
 setup(
     name='Eve',
-    version='0.5.4-dev',
+    version='0.6-dev0',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Nicola Iarocci',
