47bb7d11ec50fae706b4b655e4c5e0e74c850000
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-02-06 15:32:24 +0100

    Bump version to 0.7

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except ImportError:
 
 setup(
     name='Eve',
-    version='0.7.dev0',
+    version='0.7',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Nicola Iarocci',
