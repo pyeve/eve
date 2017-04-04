15868b19533569ebbd73f4259d416daa978c6a67
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-04-04 10:27:44 +0200

    Use official Alabaster theme instead of custom fork

--- docs/conf.py
+++ docs/conf.py
@@ -160,8 +160,7 @@ html_theme_options = {
     'logo': 'eve-sidebar.png',
     'github_user': 'pyeve',
     'github_repo': 'eve',
-    'github_banner': True,
-    'github_banner_image': 'forkme_right_green_007200.png',
+    'github_banner': 'forkme_right_green_007200.png',
     'show_powered_by': False,
 }
 # Additional templates that should be rendered to pages, maps page names to
