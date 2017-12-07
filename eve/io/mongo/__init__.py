710c34fba1f4a1e2ee7a45021f18988feed88728
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-12-07 09:18:47 +0100

    Move index creation logic in its own isolated and importable method

--- eve/io/mongo/__init__.py
+++ eve/io/mongo/__init__.py
@@ -11,6 +11,6 @@
 """
 
 # flake8: noqa
-from eve.io.mongo.mongo import Mongo, MongoJSONEncoder, create_index
+from eve.io.mongo.mongo import Mongo, MongoJSONEncoder, ensure_mongo_indexes
 from eve.io.mongo.validation import Validator
 from eve.io.mongo.media import GridFSMediaStorage
