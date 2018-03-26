9864600482c446fc4d7b523d1cf3d3fe36cbfa55
DHuan <huandzh@gmail.com>
2018-03-27 03:31:03 +0800

    Allow client projection with static exclusive projection
    Fix #1036 : allow client projection with static exclusion
    
    Changes:
    
    - Enable client projection with static exclusive projection
    - Enhance tests and docs accordingly
    
    Projection behaviors:
    
    - Static projection setting will allow client projection
    - Static inclusive projection will block sniffing
    - Static exclusive projection will allow sniffing
    - Quite weird but backwards compatible...
    
    This PR try to patch-fix the issue but not mess up with existing code. However,
    current code may require some refactoring.
    
    - NoneType projections are quite annoying and hard to maintain. Should they 
    always be dictionaries and converted to None later (for flask-pymongo)?

--- eve/tests/test_settings.py
+++ eve/tests/test_settings.py
@@ -182,6 +182,16 @@ users['resource_methods'] = ['DELETE', 'POST', 'GET']
 users['item_title'] = 'user'
 users['additional_lookup']['field'] = 'username'
 
+contacts_hide_born = copy.deepcopy(contacts)
+contacts_hide_born['url'] = 'contacts/hide_born'
+contacts_hide_born['datasource']['source'] = 'contacts'
+contacts_hide_born['datasource']['projection'] = {'born': 0}
+
+contacts_hide_media = copy.deepcopy(contacts)
+contacts_hide_media['url'] = 'contacts/hide_media'
+contacts_hide_media['datasource']['source'] = 'contacts'
+contacts_hide_media['datasource']['projection'] = {'media': 0, 'born': 0}
+
 invoices = {
     'schema': {
         'inv_number': {'type': 'string'},
@@ -339,6 +349,8 @@ DOMAIN = {
     'contacts': contacts,
     'users': users,
     'users_overseas': users_overseas,
+    'contacts_hide_born': contacts_hide_born,
+    'contacts_hide_media': contacts_hide_media,
     'invoices': invoices,
     'versioned_invoices': versioned_invoices,
     'required_invoices': required_invoices,
