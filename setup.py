d2bd01c0130ce612f4e3cb738a526c22da68e009
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-04-27 15:09:50 +0200

    WIP

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     packages=find_packages(),
     test_suite="eve.tests",
     install_requires=install_requires,
+    python_requires='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*',
     tests_require=['redis', 'testfixtures'],
     classifiers=[
         'Development Status :: 4 - Beta',
