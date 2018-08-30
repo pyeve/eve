66694216de61c613bd726260b82d71f735a25c3e
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-08-30 09:13:13 +0200

    rebuild docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.8.1.dev0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
