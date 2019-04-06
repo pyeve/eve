bd088490f29cd1182d3615d94d929fa5cbb1c03e
Shaoyu <shaoyu@life-foundry.com>
2019-04-05 20:39:49 -0500

    add HTTP status code to handle malformed DBRef

--- eve/io/mongo/mongo.py
+++ eve/io/mongo/mongo.py
@@ -495,6 +495,9 @@ class Mongo(DataLayer):
     def _change_request(self, resource, id_, changes, original, replace=False):
         """ Performs a change, be it a replace or update.
 
+        .. versionchanged:: 0.8.2
+           Return 400 if update/replace with malformed DBRef field. See #1257.
+
         .. versionchanged:: 0.6.1
            Support for PyMongo 3.0.
 
@@ -528,6 +531,13 @@ class Mongo(DataLayer):
                 400,
                 description=debug_error_message(
                     "pymongo.errors.DuplicateKeyError: %s" % e
+                ),
+            )
+        except pymongo.errors.WriteError as e:
+            abort(
+                400,
+                description=debug_error_message(
+                    "pymongo.errors.WriteError: %s" % e
                 ),
             )
         except pymongo.errors.OperationFailure as e:
