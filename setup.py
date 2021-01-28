3814fdc6154985eabd4d2044e248dac8d4b562c4
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2021-01-28 20:16:54 +0100

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
